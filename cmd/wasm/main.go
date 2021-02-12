5b6a53dc695ab7326a42065fd53e6a8924d223cb
Zebra <zebrv.apps@gmail.com>
2021-02-13 00:57:59 +0100

    improve error handling

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"errors"
 	"syscall/js"
 
 	"github.com/xzebra/unizar-calendar/internal/exports"
@@ -11,40 +12,53 @@ import (
 	"github.com/xzebra/unizar-calendar/pkg/schedules"
 )
 
+var (
+	ErrInvalidArgs     = errors.New("Invalid number of arguments passed")
+	ErrEmptySubjects   = errors.New("Empty subjects")
+	ErrEmptySchedule   = errors.New("Empty schedule")
+	ErrEmptyCalendar   = errors.New("Empty calendar data")
+	ErrInvalidSemester = errors.New("Invalid semester number")
+)
+
+func errToJS(err error) interface{} {
+	errorConstructor := js.Global().Get("Error")
+	return errorConstructor.New(err.Error())
+}
+
 func calendarWrapper() js.Func {
 	// calendarFunc(semesterNum int, subjects, schedule, exportType, calendarData string)
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) != 5 {
-			return "Invalid number of arguments passed"
+			return errToJS(ErrInvalidArgs)
 		}
 
 		// Parameters parsing
 		semesterNum := args[0].Int()
 		if semesterNum < 1 || semesterNum > 2 {
-			return "Invalid semester number"
+			return errToJS(ErrInvalidSemester)
 		}
 		subjects := args[1].String()
 		if len(subjects) == 0 {
-			return "Empty subjects"
+			return errToJS(ErrEmptySubjects)
 		}
 		schedule := args[2].String()
 		if len(schedule) < 0 {
-			return "Empty schedule"
+			return errToJS(ErrEmptySchedule)
 		}
 		var exportType exports.ExportType
 		err := exportType.Set(args[3].String())
 		if err != nil {
-			return err.Error()
+			return errToJS(err)
 		}
 		calendarData := args[4].String()
 		if len(calendarData) == 0 {
-			return "Empty calendar data"
+			return errToJS(ErrEmptyCalendar)
 		}
 
 		// Get semester from github stored data
 		sem, err := semester.NewSemesterFromData([]byte(calendarData))
 		if err != nil {
-			return err.Error()
+			return errToJS(err)
 		}
 
 		// Parse user input
@@ -53,14 +67,14 @@ func calendarWrapper() js.Func {
 			Schedule: schedule,
 		})
 		if err != nil {
-			return err.Error()
+			return errToJS(err)
 		}
 
 		// Generate merged data from user input and calendar data from
 		// github.
 		data, err := semester.NewData(sem, parsed, semesterNum)
 		if err != nil {
-			return err.Error()
+			return errToJS(err)
 		}
 
 		// Export with user export option
