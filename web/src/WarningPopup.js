26575ebd10795745200cf34e2266dfda9bed7613
xzebra <zebrv.apps@gmail.com>
2022-01-24 21:46:15 +0200

    add imports

--- web/src/WarningPopup.js
+++ web/src/WarningPopup.js
@@ -1,5 +1,9 @@
 import React from "react";
+
+import { useTranslation } from "react-i18next";
+
 import { PopupboxManager } from "react-popupbox";
+import ReactMarkdown from 'react-markdown';
 import Popup from './Popup'
 
 function WarningPopup({ warningID }) {
